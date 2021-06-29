57309189038a097747756a9dae03e1b6fb052e4f
ilya07092000 <ischenko2012@gmail.com>
2021-06-29 11:30:28 +0300

    feat: boards and board data

---
+++ src/mock/boards.ts
@@ -0,0 +1,17 @@
+export const boards = [
+  {
+    id: 1,
+    title: 'Board 1',
+    date: 'date',
+  },
+  {
+    id: 2,
+    title: 'Board 2',
+    date: 'date',
+  },
+  {
+    id: 3,
+    title: 'Board 3',
+    date: 'date',
+  },
+];
---
+++ src/mock/boardsData.ts
@@ -0,0 +1,20 @@
+export const boardsData = [
+  {
+    id: 1,
+    colums: [
+      {
+        title: 'Todo 1',
+        items: [
+          {
+            id: 1,
+            title: 'Todo in col',
+          },
+          {
+            id: 2,
+            title: 'Todo in col',
+          },
+        ],
+      },
+    ],
+  },
+];
