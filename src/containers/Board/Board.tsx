3d7d58b58d2083e6a0341c521967d0237d86c0cb
ilya07092000 <ischenko2012@gmail.com>
2021-07-14 20:23:31 +0300

    feat: drag drop tasks

--- src/containers/Board/Board.tsx
+++ src/containers/Board/Board.tsx
@@ -4,12 +4,15 @@ import boardStore from '../../store/boards';
 import CreateCol from '../../components/CreateCol/CreateCol';
 import BoardCol from '../BoardCol/BoardCol';
 import { observer } from 'mobx-react-lite';
+import { ITask, IBoardCol } from '../../types';
 
 import styles from './Board.module.scss';
 
 const Board = () => {
   let location = useLocation();
   const [board, setBoard] = useState<any>({});
+  const [currentCol, setCurrentCol] = useState<number | null>(null);
+  const [currentItem, setCurrentItem] = useState<ITask | null>(null);
 
   useEffect(() => {
     const path: string[] = location.pathname.split('/');
@@ -28,13 +31,118 @@ const Board = () => {
     boardStore.createNewCol(board.id, col);
   };
 
+  const dropHanlder = (e: React.SyntheticEvent, task: ITask, colId: number) => {
+    e.preventDefault();
+    (e.target as HTMLElement).style.boxShadow = 'none';
+
+    if (task.id !== currentItem!.id || currentCol !== colId) {
+      const currentColIndex: number = board.columns.findIndex(
+        (col: IBoardCol) => col.id === currentCol
+      );
+      const currentTaskIndex: number = board.columns[
+        currentColIndex
+      ].list.findIndex((task: ITask) => task.id === currentItem!.id);
+
+      const targetColIndex: number = board.columns.findIndex(
+        (col: IBoardCol) => col.id === colId
+      );
+      const targetTaskIndex: number = board.columns[
+        targetColIndex
+      ].list.findIndex((t: ITask) => t.id === task.id);
+
+      boardStore.deleteTaskById(currentColIndex, currentTaskIndex, board.id);
+      boardStore.addTaskById(
+        targetColIndex,
+        targetTaskIndex,
+        currentItem!,
+        board.id
+      );
+    }
+  };
+
+  const dragEndColHanlder = (
+    e: React.SyntheticEvent,
+    colId: number,
+    isTask: number
+  ) => {
+    if (isTask) return;
+
+    const currentColIndex: number = board.columns.findIndex(
+      (col: IBoardCol) => col.id === currentCol
+    );
+
+    const currentTaskIndex: number = board.columns[
+      currentColIndex
+    ].list.findIndex((task: ITask) => task.id === currentItem!.id);
+
+    const targetColIndex: number = board.columns.findIndex(
+      (col: IBoardCol) => col.id === colId
+    );
+
+    boardStore.deleteTaskById(currentColIndex, currentTaskIndex, board.id);
+    boardStore.addTaskById(targetColIndex, 0, currentItem!, board.id);
+  };
+
+  const dragOverHandler = (
+    e: React.SyntheticEvent,
+    task: ITask,
+    colId: number
+  ) => {
+    e.preventDefault();
+
+    if ((e.target as HTMLElement).classList.contains('board-item')) {
+      (e.target as HTMLElement).style.boxShadow = '0 5px 4px rgba(0, 0, 0, 1)';
+    }
+  };
+
+  const dragStartHandler = (
+    e: React.SyntheticEvent,
+    task: ITask,
+    colId: number
+  ) => {
+    setCurrentCol(colId);
+    setCurrentItem(task);
+  };
+
+  const dragLeaveHanlder = (e: React.SyntheticEvent) => {
+    (e.target as HTMLElement).style.boxShadow = 'none';
+  };
+
+  const dragEndHandler = (e: React.SyntheticEvent) => {
+    // (e.target as HTMLElement).style.boxShadow = 'none';
+  };
+
   return (
     <div className={styles.wrapper}>
       <p className={styles.pageTitle}>{board.title}</p>
       <div className={styles.content}>
         {board.columns
-          ? board.columns.map((col: any) => (
-              <BoardCol key={col.id} data={col} boardId={board.id} />
+          ? board.columns.map((col: IBoardCol) => (
+              <BoardCol
+                dragOverHandler={(
+                  e: React.DragEvent,
+                  task: ITask,
+                  id: number
+                ) => dragOverHandler(e, task, id)}
+                dragLeaveHanlder={(e: React.DragEvent) => dragLeaveHanlder(e)}
+                dragStartHandler={(
+                  e: React.DragEvent,
+                  task: ITask,
+                  id: number
+                ) => dragStartHandler(e, task, id)}
+                dragEndHandler={(e: React.DragEvent) => dragEndHandler(e)}
+                dropHanlder={(e: React.DragEvent, task: ITask, id: number) =>
+                  dropHanlder(e, task, id)
+                }
+                dragEndColHanlder={(
+                  e: React.DragEvent,
+                  colId: number,
+                  isTask: number
+                ) => dragEndColHanlder(e, colId, isTask)}
+                key={col.id}
+                data={col}
+                boardId={board.id}
+              />
             ))
           : null}
         <div className={styles.contentItem}>
--- src/containers/BoardCol/BoardCol.tsx
+++ src/containers/BoardCol/BoardCol.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import Input from '../../components/Input/Input';
 import Button from '../../components/Button/Button';
 import boardStore from '../../store/boards';
@@ -10,9 +10,28 @@ import styles from './BoardCol.module.scss';
 type BoardColProps = {
   data: any;
   boardId: number;
+  dragOverHandler: (e: React.DragEvent, task: ITask, id: number) => void;
+  dragLeaveHanlder: (e: React.DragEvent) => void;
+  dragStartHandler: (e: React.DragEvent, task: ITask, id: number) => void;
+  dragEndHandler: (e: React.DragEvent) => void;
+  dropHanlder: (e: React.DragEvent, task: ITask, id: number) => void;
+  dragEndColHanlder: (
+    e: React.DragEvent,
+    colId: number,
+    isTask: number
+  ) => void;
 };
 
-const BoardCol = ({ data, boardId }: BoardColProps) => {
+const BoardCol = ({
+  data,
+  boardId,
+  dragOverHandler,
+  dragLeaveHanlder,
+  dragStartHandler,
+  dragEndHandler,
+  dropHanlder,
+  dragEndColHanlder,
+}: BoardColProps) => {
   const inputRef = useRef<HTMLInputElement | null>(null);
 
   const addNewTask = (): void => {
@@ -29,7 +48,13 @@ const BoardCol = ({ data, boardId }: BoardColProps) => {
 
   return (
     <div className={styles.contentItem}>
-      <div className={styles.colItem}>
+      <div
+        className={styles.colItem}
+        onDragOver={(e) => {
+          e.preventDefault();
+        }}
+        onDrop={(e) => dragEndColHanlder(e, data.id, data.list.length)}
+      >
         <p className={styles.colTitle}>{data.title}</p>
         <div className={styles.addTask}>
           <Input placeholder="123" ref={inputRef} />
@@ -38,7 +63,18 @@ const BoardCol = ({ data, boardId }: BoardColProps) => {
           </div>
         </div>
         {data.list.map((task: ITask) => (
-          <div key={task.id} className={styles.colText}>{task.text}</div>
+          <div
+            draggable="true"
+            key={task.id}
+            className={[styles.colText, 'board-item'].join(' ')}
+            onDragOver={(e) => dragOverHandler(e, task, data.id)}
+            onDragLeave={(e) => dragLeaveHanlder(e)}
+            onDragStart={(e) => dragStartHandler(e, task, data.id)}
+            onDragEnd={(e) => dragEndHandler(e)}
+            onDrop={(e) => dropHanlder(e, task, data.id)}
+          >
+            {task.text}
+          </div>
         ))}
       </div>
     </div>
