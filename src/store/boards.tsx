1d81a1098cb5f00859de710ce711bc0e57aa0b6f
ilya07092000 <ischenko2012@gmail.com>
2021-07-14 20:23:16 +0300

    feat: change col store

--- src/store/boards.tsx
+++ src/store/boards.tsx
@@ -11,15 +11,18 @@ class Boards {
 
   addBoard(board: IBoard) {
     this.boards.push(board);
+    this.updateLocalStorage();
   }
 
   editBoard(id: number, title: string) {
     const currBoard: any = this.boards.find((b) => b.id === id);
     currBoard.title = title;
+    this.updateLocalStorage();
   }
 
   deleteBoard(id: number) {
     this.boards = this.boards.filter((b) => b.id !== id);
+    this.updateLocalStorage();
   }
 
   getBoardById(id: number): IBoard | undefined {
@@ -33,11 +36,39 @@ class Boards {
       const col = boardById.columns.find((col: any) => col.id === columnId);
       col.list.push(task);
     }
+    this.updateLocalStorage();
   }
 
   createNewCol(boardId: number, col: any) {
     const boardById: IBoard | undefined = this.getBoardById(boardId);
     boardById?.columns.push(col);
+    this.updateLocalStorage();
+  }
+
+  deleteTaskById(colIndex: number, taskIndex: number, boardId: number) {
+    const currBoard = this.getBoardById(boardId);
+    if (currBoard) {
+      currBoard.columns[colIndex].list.splice(taskIndex, 1);
+    }
+    this.updateLocalStorage();
+  }
+
+  addTaskById(
+    colIndex: number,
+    dropIndex: number,
+    task: ITask,
+    boardId: number
+  ) {
+    const currBoard = this.getBoardById(boardId);
+
+    if (currBoard) {
+      currBoard.columns[colIndex].list.splice(dropIndex + 1, 0, task);
+    }
+    this.updateLocalStorage();
+  }
+
+  updateLocalStorage() {
+    localStorage.setItem('trelloBoards', JSON.stringify(this.boards));
   }
 }
 
